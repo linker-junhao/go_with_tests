309375a288e2540f73bc892306f58fc73215806f
linkerGitHub <linker-junhao@outlook.com>
2020-01-07 22:21:49 +0800

    struct 和 interface 部分的练习 - 表格驱动测试实践

--- structAndInterface/struct_interface_test.go
+++ structAndInterface/struct_interface_test.go
@@ -30,29 +30,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkShape := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTest := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{
-			Width:  12,
-			Height: 6,
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.want)
 		}
-		want := 72.0
-
-		checkShape(t, rectangle, want)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{Radius: 10}
-		want := 314.1592653589793
-
-		checkShape(t, circle, want)
-	})
+	}
 }
