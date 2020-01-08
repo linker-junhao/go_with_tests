c60dabff93ac0ab9b33d68658478cd60213e89cd
linkerGitHub <linker-junhao@outlook.com>
2020-01-08 15:48:14 +0800

    完成了DI部分的练习，虽然还是不是很理解

---
+++ main/mian.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request){
+	dependencyInjection.Greet(w, "world")
+}
+
+func main()  {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+}*/
+
+func main() {
+
+}
